e3af1fc3c376605681833c0708ec8de12f826cc1
Jhonatha Nunes <jhon@MacBook-Air-de-Jhonatha.local>
2026-08-05 10:24:23 -0300

    fix: acessos

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/request';
+import type { NextRequest } from 'next/server';
 import { createServerClient } from '@supabase/ssr';
 
 export async function middleware(request: NextRequest) {
